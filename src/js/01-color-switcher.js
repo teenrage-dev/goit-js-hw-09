66b54279cfb7a121f9131d796869381709a766ee
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-06-01 21:56:12 +0300

    done 1-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+const refs = {
+    startColorSwitcherBtn: document.querySelector('button[data-start]'),
+    stopColorSwitcherBtn: document.querySelector('button[data-stop]'),
+}
+
+refs.startColorSwitcherBtn.addEventListener('click', changeColor);
+refs.stopColorSwitcherBtn.addEventListener('click', stopSwitcher);
+
+// Styles
+
+refs.startColorSwitcherBtn.style.backgroundColor = '#ebebeb';
+refs.startColorSwitcherBtn.style.padding = '28px 44px';
+refs.startColorSwitcherBtn.style.fontSize = '40px';
+refs.stopColorSwitcherBtn.style.backgroundColor = '#ebebeb';
+refs.stopColorSwitcherBtn.style.padding = '28px 44px';
+refs.stopColorSwitcherBtn.style.fontSize = '40px';
+
+
+
+
+
+let timerId = null;
+let isActive = false;
+function changeColor() {
+    if (isActive) {
+        return;
+    }
+    
+    timerId = setInterval(() => {
+        const color = '#' + Math.random().toString(16).slice(2, 8);
+        document.body.style.backgroundColor = color;
+    }, 1000);
+    isActive = true;
+    refs.startColorSwitcherBtn.style.opacity = '.5';
+    refs.stopColorSwitcherBtn.style.opacity = '1';
+}
+
+function stopSwitcher() {
+    isActive = false;
+    refs.startColorSwitcherBtn.style.opacity = '1';
+    refs.stopColorSwitcherBtn.style.opacity = '.5';
+    return clearInterval(timerId);
+}
+
 
